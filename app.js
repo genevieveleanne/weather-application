d035c2b56d4bf493d049605a84dad126ca9869a8
Genevieve Leanne Dominguez <genevieveleanned25@gmail.com>
2023-10-18 15:14:49 -0400

    Edited a JS comment and added some styling

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ dateDisplay.innerHTML = formatDate(currentDate);
 let apiKey = "1bac80fa0c32ft537387a483f19bf3fo";
 let apiBase = "https://api.shecodes.io/weather/v1/";
 
-// Display Current Weather for London on load
+// Display Current Weather for London on Page Load
 function searchCity(city) {
   let currentWeather = `${apiBase}/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(currentWeather).then(displayUserInput);
