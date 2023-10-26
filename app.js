05e8a4b41ead4b2cb6ede805ad640c8276afe1dd
Genevieve Leanne Dominguez <genevieveleanned25@gmail.com>
2023-10-26 10:47:55 -0400

    Integrated API to display forecast on page

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ function searchCity(city) {
   axios.get(currentWeather).then(displayUserInput);
 
   let forecast = `${apiBase}/forecast?query=${city}&key=${apiKey}&units=imperial`;
-  axios.get(forecast).then(displayFiveForecast);
+  axios.get(forecast).then(displayForecast);
 }
 
 searchCity("Atlanta");
@@ -159,35 +159,28 @@ let fahrenheitButton = document.querySelector("#fahrenheit-button");
 fahrenheitButton.addEventListener("click", displayFahrenheitTemp);
 
 //5-Day Forecast
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast-row");
 
   let forecastHTML = `<div class="row forecast-row">`;
 
-  function showForecast(day) {
+  function showForecast(forecastDay) {
     forecastHTML =
       forecastHTML +
       `<div class="col">
-    <h5>${day}</h5>
-    <span class="forecast-description">Clear Sky</span>
-    <img src="" alt="Weather Icon" />
+    <h5>${forecastDay.time}</h5>
+    <span class="forecast-description">${forecastDay.condition.description}</span>
+    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png" alt="Weather Icon"/>
     <ul class="forecast-list">
-      <li class="forecast-temperature">High: 80°</li>
-      <li class="forecast-temperature">Low: 75°</li>
+      <li class="forecast-temperature">High: ${forecastDay.temperature.maximum}°</li>
+      <li class="forecast-temperature">Low: ${forecastDay.temperature.minimum}°</li>
     </ul>
     </div>`;
   }
 
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(showForecast);
+  let forecast = response.data.daily;
+  forecast.forEach(showForecast);
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-}
-
-displayForecast();
-
-//Console Log Forecast
-function displayFiveForecast(response) {
-  console.log(response.data);
 }
