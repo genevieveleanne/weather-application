99bd2dfda1351182a10a969162660d5b5e2561f9
Genevieve Leanne Dominguez <genevieveleanned25@gmail.com>
2025-07-14 21:07:39 -0400

    Added axios and included SheCodes API

--- weather.js
+++ weather.js
@@ -1,4 +1,3 @@
-//Step 3: Integrate API
 //Step 4: Display user's city, current temp, description, wind speed, and humidity
 
 //GET CURRENT DATE
@@ -42,15 +41,29 @@ let today = new Date();
 let currentDate = document.querySelector("#current-date");
 currentDate.innerHTML = formatDate(today);
 
-//CREATE SEARCH ENGINE
+//API KEY & BASEPOINT
+let apiKey = "1bac80fa0c32ft537387a483f19bf3fo";
+let apiBase = "https://api.shecodes.io/weather/v1/";
+
+//GET USER'S CITY WEATHER DATA
+function searchUserCity(userCity) {
+  let currentWeather = `${apiBase}current?query=${userCity}&key=${apiKey}&units=imperial`;
+  axios.get(currentWeather).then(displayCurrentWeather);
+}
+
+//USER SUBMITS FORM
 function getUserCity(event) {
   event.preventDefault();
 
   let userCity = document.querySelector("#city-search");
-  let h1 = document.querySelector("h1");
 
-  h1.innerHTML = `${userCity.value}`;
+  searchUserCity(userCity.value);
 }
 
-let searchForm = document.querySelector("#search-form");
+let searchForm = document.querySelector("#city-search-form");
 searchForm.addEventListener("submit", getUserCity);
+
+//DISPLAY CURRENT WEATHER
+function displayCurrentWeather(response) {
+  console.log(response);
+}
